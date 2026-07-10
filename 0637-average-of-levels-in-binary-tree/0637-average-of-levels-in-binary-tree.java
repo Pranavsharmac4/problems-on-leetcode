dcdb4ba5482582397bb80ce6fb0a36daf9bc44b7
Pranav sharma <pranavsharma1242006@gmail.com>
2026-07-10 17:12:07 +0530

    Time: 2 ms (97.64%), Space: 48.5 MB (57.37%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            double levelSum = 0;
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                levelSum += currentNode.val;
+                
+                if (currentNode.left != null) queue.add(currentNode.left);
+                if (currentNode.right != null) queue.add(currentNode.right);
+            }
+            
+            result.add(levelSum / levelSize);
+        }
+
+        
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
